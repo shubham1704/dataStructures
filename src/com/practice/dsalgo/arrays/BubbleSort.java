71d2740ebb7d71fbe4ee07e1930fefbe088f5163
sjai0003 <shubham.jaiswal@synchronoss.com>
2021-03-19 03:48:03 +0530

    Bubble Sort with optimized number of loops

--- src/com/practice/dsalgo/arrays/BubbleSort.java
+++ src/com/practice/dsalgo/arrays/BubbleSort.java
@@ -3,7 +3,34 @@ package com.practice.dsalgo.arrays;
 public class BubbleSort {
 
   public static void main(String[] args) {
+    int a[] = {-3, 0, 1, 2, 5, 544};
+    int temp;
+    int countJ = 0;
+    boolean sorted = false;
 
+    for (int i = 0; i < a.length - 1; i++) {
+      sorted = true;
+      for (int j = 0; j < a.length - 1-i; j++) {
+        countJ++;
+        if (a[j] > a[j + 1]) {
+          temp = a[j];
+          a[j] = a[j + 1];
+          a[j + 1] = temp;
+          sorted = false; // in case the array has already been sorted then we don't have to come back to check the elements
+        }
+      }
+      if (sorted)
+      {
+        break;
+      }
+    }
+
+    System.out.print("{ ");
+    for (int ar : a) {
+      System.out.print(ar + ", ");
+    }
+    System.out.println("}");
+
+    System.out.println("\nCount of J = " + countJ);
   }
-
 }
