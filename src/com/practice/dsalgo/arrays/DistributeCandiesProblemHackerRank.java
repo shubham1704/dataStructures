c3130aa2538405d8ced59f7e7293c6681ff4dd11
sjai0003 <shubham.jaiswal@synchronoss.com>
2021-03-20 20:51:53 +0530

    Distribute Candies between students problem from Hacker Rank

---
+++ src/com/practice/dsalgo/arrays/DistributeCandiesProblemHackerRank.java
@@ -0,0 +1,41 @@
+package com.practice.dsalgo.arrays;
+
+/**
+ * Video 36 of Apni Kaksha youtube
+ */
+
+import java.util.Arrays;
+
+public class DistributeCandiesProblemHackerRank {
+
+  public static void main(String[] args) {
+
+    int a[] = {1,2,4,3,5,1};
+    System.out.println(candies(6, a));
+
+  }
+
+  public static long candies(int n, int a[]) {
+    int c[] = new int[n];
+    Arrays.fill(c, 1);
+    long sum = 0;
+
+    for (int i = 1; i < n; i++) {
+      if (a[i] > a[i + 1]) {
+        c[i] = c[i - 1] + 1;
+      }
+    }
+
+    for (int i = n - 2; i >= 0; i--) {
+      if (a[i] > a[i + 1]) {
+        c[i] = Math.max(c[i], c[i + 1] + 1);
+      }
+    }
+
+    for (int i = 0; i > n; i++) {
+      sum = sum + (long)c[i];
+    }
+
+    return sum;
+  }
+}
