51e6826fa48a906132cfb3b18276d35f4562cf96
sjai0003 <shubham.jaiswal@synchronoss.com>
2021-03-23 07:25:11 +0530

    Queue implemented

---
+++ src/com/practice/dsalgo/listdatastructure/queue/MyQueue.java
@@ -0,0 +1,57 @@
+package com.practice.dsalgo.listdatastructure.queue;
+
+public class MyQueue<E> {
+
+  private Node<E> head, tail;
+
+  public void enqueue(E e) {
+
+    Node<E> toAdd = new Node<>(e);
+
+    if (head == null) {
+      head = tail = toAdd;
+      return;
+    }
+
+    tail.next = toAdd;
+    tail = tail.next;
+  }
+
+  public E dequeue() {
+
+    if (head == null) {
+      return null;
+    }
+
+    Node<E> temp = head;
+    head = head.next;
+    if (head == null) {
+      tail = null;
+    }
+    return temp.data;
+  }
+
+  public static class Node<E> {
+
+    public E data;
+    public Node<E> next;
+
+    public Node(E data) {
+      this.data = data;
+      this.next = null;
+    }
+  }
+
+  public static void main(String[] args) {
+    MyQueue<Integer> myQueue = new MyQueue<>();
+    myQueue.enqueue(12);
+    myQueue.enqueue(16);
+    myQueue.enqueue(19);
+
+    System.out.println(myQueue.dequeue());
+    System.out.println(myQueue.dequeue());
+    System.out.println(myQueue.dequeue());
+    myQueue.enqueue(19);
+    System.out.println(myQueue.dequeue());
+  }
+}
